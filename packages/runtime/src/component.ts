af568940450b8580d3106e8f6dd4ee0e19497e83
uhyo <uhyo@uhy.ooo>
2020-10-22 22:34:08 +0900

    feat(runtime): add component() function

---
+++ packages/runtime/src/component.ts
@@ -0,0 +1,31 @@
+import React from "react";
+import { wc } from "react-wc";
+
+export type ComponentOptions<SlotName extends string> = {
+  /**
+   * HTML string to be rendered in Shadow DOM.
+   */
+  readonly html: string;
+  /**
+   * Name of slots accepted by this component.
+   */
+  readonly slots: readonly SlotName[];
+  /**
+   * Name of this component.
+   */
+  readonly name: string;
+};
+
+type CastellaComponentProps<SlotName extends string> = string extends SlotName
+  ? {}
+  : { [N in Exclude<SlotName, "children" | "">]?: React.ReactNode };
+
+export type CastellaComponent<
+  SlotName extends string
+> = React.FunctionComponent<CastellaComponentProps<SlotName>>;
+
+export function component<SlotName extends string>(
+  obj: ComponentOptions<SlotName>
+): CastellaComponent<SlotName> {
+  return wc(obj);
+}
